8ccb3bca3460ef5b3b1f84221f8886565865a1d2
Tim Hanson <tbh20715@gmail.com>
2021-09-15 09:34:06 -0700

    wip

--- main.ts
+++ main.ts
@@ -1,11 +1,37 @@
-import twilio from 'twilio';
+import * as twilio from 'twilio';
+import * as puppeteer from 'puppeteer';
 
 
 async function scan()
 {
-    sendMessage()
+ 
+    setInterval(async () => {
+        var result = await loadWebpage('https://www.tesla.com/inventory/new/my?arrangeby=plh&zip=92109&range=200');
+        if (result)
+        {
+            sendMessage("Found new inventory: " + result);
+        }
+    }, 1000);
 }
 scan();
+
+async function loadWebpage(uri: string): Promise<string>
+{
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(uri);
+
+    let result = '';
+    await page.waitForSelector('.result-purchase-price', { timeout: 2000 });
+    const query = await page.$('.result-purchase-price');
+    if (!!query)
+    {
+        result = await page.evaluate(el => el.textContent, query);
+    }
+
+    await browser.close();
+    return result;
+}
 
 async function sendMessage(message: string)
 {
@@ -14,23 +40,24 @@ async function sendMessage(message: string)
     const accountSid = process.env.TWILIO_ACCOUNT_SID;
     const authToken = process.env.TWILIO_AUTH_TOKEN;
     const recipient = process.env.TWILIO_RECIPIENT;
+    const sender = process.env.TWILIO_SENDER;
 
     // require the Twilio module and create a REST client
     // const client = require('twilio')(accountSid, authToken);
-    const client = twilio(accountSid, authToken);
+    const client = (<any>twilio)(accountSid, authToken);
 
     try
     {
         await client.messages
         .create({
-            to: ,
-            from: '+15017122661',
+            to: recipient ?? '',
+            from: sender ?? '',
             body: message,
         });
     }
     catch(err: any)
     {
-        console.error('Failed to send message: ' + message);
+        console.error('Failed to send message: ' + err);
     }
-    console.log('Successfully sent message: ' + message);
+    console.log('Successfully sent message!');
 }
